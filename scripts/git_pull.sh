3c1a8620ffb7b84884d0450b99305032f571d0c7
thaliamontreux <thaliamontreux@gmail.com>
2025-12-21 17:17:37 -0600

    Auto commit 2025-12-21 17:17:37

--- scripts/git_pull.sh
+++ scripts/git_pull.sh
@@ -1,15 +1,7 @@
 git pull
 echo "Restarting Web Server"
 systemctl restart pentavision-web
-echo "Restarting Blocklist Server"
-systemctl restart pentavision-blocklist
-echo "Restarting Log Server"
-systemctl restart pentavision-logserver
-sleep 5
 echo "Restarting Video Worker"
 systemctl restart pentavision-video-worker
-sleep 10
-systemctl status pentavision-web
-systemctl status pentavision-blocklist
-systemctl status pentavision-logserver
-systemctl status pentavision-video-worker
+echo "All Done Have a nice day"
+
