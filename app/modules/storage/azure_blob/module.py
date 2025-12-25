c8c1be315850501eef0032964e34db6c694a7fd1
thaliamontreux <thaliamontreux@gmail.com>
2025-12-25 02:13:29 -0600

    Auto commit 2025-12-25 02:13:28

--- app/modules/storage/azure_blob/module.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from app import storage_providers as _storage_providers  # noqa: F401
--- app/modules/storage/s3/module.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-
-# Importing storage_providers registers the CSAL factory for s3.
-from app import storage_providers as _storage_providers  # noqa: F401
